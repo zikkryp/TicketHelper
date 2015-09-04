f415bc86cbf293ee0039e7c2fb6ae5c17ac98e8a
zikkryp <zikkryp@gmail.com>
2015-09-04 06:37:29 +0300

    staging

---
+++ TicketHelper/Abstract/IHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketHelper.Models;
+
+namespace TicketHelper.Abstract
+{
+    public interface IHistoryRepository
+    {
+        IList<Ticket> Tickets { get; }
+    }
+}
